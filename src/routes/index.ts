b342ee1725a514b999ad0c75f10a778bdbb9a4ca
DESKTOP-JJLA00B\moses <moses>
2026-06-17 21:52:26 +0530

    register auth router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,11 @@ import usersRouter from "./users";
 import restaurantsRouter from "./restaurants";
 import menuItemsRouter from "./menuItems";
 import ordersRouter from "./orders";
+import authRouter from "./auth";
 
 const router = Router();
 
+router.use(authRouter);
 router.use(usersRouter);
 router.use(restaurantsRouter);
 router.use(menuItemsRouter);
